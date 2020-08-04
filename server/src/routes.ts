c84f04ba2265f95c2b8d6782727194d15def091e
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-08-04 16:43:19 -0300

    listando total de conexões

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -12,5 +12,6 @@ routes.get('/classes', classesController.index);
 
 
 routes.post('/connections', connectionsController.create);
+routes.get('/connections', connectionsController.index);
 
 export default routes;
