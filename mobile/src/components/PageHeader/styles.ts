10607e4edec05be890433db168fe1fce2ddf6d9e
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-08-06 18:48:38 -0300

    criando componente PageHeader

---
+++ mobile/src/components/PageHeader/styles.ts
@@ -0,0 +1,25 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: '#8257e5',
+		padding: 40,
+	},
+
+	topBar: {
+		alignItems: 'center',
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+
+	title: {
+		color: '#fff',
+		fontFamily: 'Archivo_700Bold',
+		fontSize: 24,
+		lineHeight: 32,
+		maxWidth: 160,
+		marginVertical: 40
+	}
+});
+
+export default styles;
