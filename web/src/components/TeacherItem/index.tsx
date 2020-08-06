1ce1312c9385ef851b92e7490601276c520d7a83
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-08-06 01:24:33 -0300

    criando conexão ao entrar em contato com o professor

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
 export interface Teacher {
 	id: number;
@@ -19,6 +20,12 @@ interface TeacherItemProps {
 }
 
 const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+	function createNewConnection() {
+		api.post('/connections', {
+			user_id: teacher.id
+		});
+	}
+
 	return (
 		<article className="teacher-item">
 			<header>
@@ -43,7 +50,7 @@ const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
 					<strong>R$ {teacher.cost}</strong>
 				</p>
 
-				<a href={`https://wa.me/${teacher.whatsapp}`} target="_blank" rel="noopener noreferrer">
+				<a href={`https://wa.me/${teacher.whatsapp}`} onClick={createNewConnection} target="_blank" rel="noopener noreferrer">
 					<img src={whatsappIcon} alt="Whatsapp" />
 					Entrar em contato
 				</a>
